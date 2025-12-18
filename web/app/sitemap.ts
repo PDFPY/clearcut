36c5f6685e2f8859153c1dd48f071dac42948369
Brandon Walter <brandon@users.noreply.github.com>
2025-12-17 23:41:33 -0600

    Add sitemap

--- web/app/sitemap.ts
+++ web/app/sitemap.ts
@@ -5,22 +5,26 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: "https://clearcuttools.com",
       lastModified: new Date(),
-    },
-    {
-      url: "https://clearcuttools.com/remove-background",
-      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
     },
     {
       url: "https://clearcuttools.com/privacy",
       lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.3,
     },
     {
       url: "https://clearcuttools.com/terms",
       lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.3,
     },
     {
       url: "https://clearcuttools.com/contact",
       lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.2,
     },
   ];
 }
